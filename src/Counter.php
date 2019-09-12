47169f0c7600418b39ea077877bd5211ca546d35
Jamie Rodway <jamie.rodway@oneiota.co.uk>
2019-09-12 11:53:22 +0100

    Allow application of prometheus labels to metrics

--- src/Counter.php
+++ src/Counter.php
@@ -16,11 +16,11 @@ class Counter {
         return self::$instance;
     }
 
-    public static function updateStats($stats, $delta=1, $sampleRate=1, $metric='c') {
+    public static function updateStats($stats, $delta=1, $sampleRate=1, $metric='c', $tags = []) {
         if (is_null(self::$instance)) {
             return;
         }
-        self::$instance->updateStats($stats, $delta, $sampleRate, $metric);
+        self::$instance->updateStats($stats, $delta, $sampleRate, $metric, $tags);
     }
 
 	/**
@@ -28,9 +28,10 @@ class Counter {
      *
      * @param string|array $stats The metric(s) to set.
      * @param float $time The elapsed time (ms) to log
+     * @param array $tags list of tags to be applied to the metric
      **/
-    public static function timing($stats, $time) {
-        self::updateStats($stats, $time, 1, 'ms');
+    public static function timing($stats, $time, $tags =[]) {
+        self::updateStats($stats, $time, 1, 'ms', $tags);
     }
 
     /**
@@ -38,9 +39,10 @@ class Counter {
      *
      * @param string|array $stats The metric(s) to set.
      * @param float $value The value for the stats.
+     * @param array $tags list of tags to be applied to the metric
      **/
-    public static function gauge($stats, $value) {
-        self::updateStats($stats, $value, 1, 'g');
+    public static function gauge($stats, $value, $tags =[]) {
+        self::updateStats($stats, $value, 1, 'g', $tags);
     }
 
     /**
@@ -55,10 +57,11 @@ class Counter {
      * with each request with a key of "uniques" (or similar).
      *
      * @param string|array $stats The metric(s) to set.
+     * @param array $tags list of tags to be applied to the metric
      * @param float $value The value for the stats.
      **/
-    public static function set($stats, $value) {
-        self::updateStats($stats, $value, 1, 's');
+    public static function set($stats, $value, $tags =[]) {
+        self::updateStats($stats, $value, 1, 's', $tags);
     }
 
     /**
@@ -66,10 +69,11 @@ class Counter {
      *
      * @param string|array $stats The metric(s) to increment.
      * @param float|1 $sampleRate the rate (0-1) for sampling.
+     * @param array $tags list of tags to be applied to the metric
      * @return boolean
      **/
-    public static function increment($stats, $sampleRate=1) {
-        self::updateStats($stats, 1, $sampleRate, 'c');
+    public static function increment($stats, $sampleRate=1, $tags =[]) {
+        self::updateStats($stats, 1, $sampleRate, 'c', $tags);
     }
 
     /**
@@ -77,9 +81,10 @@ class Counter {
      *
      * @param string|array $stats The metric(s) to decrement.
      * @param float|1 $sampleRate the rate (0-1) for sampling.
+     * @param array $tags list of tags to be applied to the metric
      * @return boolean
      **/
-    public static function decrement($stats, $sampleRate=1) {
-        self::updateStats($stats, -1, $sampleRate, 'c');
+    public static function decrement($stats, $sampleRate=1, $tags =[]) {
+        self::updateStats($stats, -1, $sampleRate, 'c', $tags);
     }
 }
